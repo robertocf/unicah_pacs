7d5cbc86a87a263dfd888723b3edf2c8dd6f4d63
Roberto Carvalho Fernandes <roberto.carvalho764@gmail.com>
2026-03-03 15:43:22 -0400

    Debul false

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from routes import *
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
